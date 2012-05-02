0e1cf0c83f6b66c0c658113fe7ca2ceb62ac628c
Horace Ko <horace@airbnb.com>
2012-05-02 11:13:44 -0700

    Added several other options to the leaderboard.

--- blamer.rb
+++ blamer.rb
@@ -31,7 +31,8 @@ ERROR_URL =  "https://airbnb.airbrake.io/errors/%s.xml?auth_token=#{AUTH_TOKEN}"
 
 options = {
   :verbose => true,
-  :pages => 3
+  :pages => 3,
+  :reverse => false
 }
 
 OptionParser.new do |opts|
@@ -41,12 +42,24 @@ OptionParser.new do |opts|
     options[:file] = f
   end
 
-  opts.on("-v", "Increase verbosity") do |v|
+  opts.on("-v", "--verbose", "Increase verbosity") do |v|
     options[:verbose] = true
   end
 
   opts.on("--pages PAGES", "Specify the number of Airbrake error pages to parse (default 3)") do |p|
     options[:pages] = p.to_i
+  end
+
+  opts.on('-r', "--reverse", "Reverse sorting order (lower rankings displayed first)") do |r|
+    options[:reverse] = true
+  end
+
+  opts.on('--top NUMBER', "Display the top <NUMBER> of results") do |n|
+    options[:number] = n.to_i 
+  end
+
+  opts.on('--presenter', '-p', "Enable presenter mode. Users must hit a keyboard key between printing each item.") do |p|
+    options[:presenter] = true
   end
 end.parse!
 
@@ -56,7 +69,7 @@ blames = {}
 # PARSING
 ##
 
-puts "Grabbing list of errors from Airbrake..." if options[:verbose]
+puts "Grabbing list of errors from #{options.has_key?(:file) ? "file" : "Airbrake"}..." if options[:verbose]
 
 (1..options[:pages]).each do |page|
   puts "Getting page #{page}..." if options[:verbose]
@@ -73,8 +86,12 @@ puts "Grabbing list of errors from Airbrake..." if options[:verbose]
     puts "Grabbing backtrace for error #{e.id}..." if options[:verbose]
 
     # now grab the backtraces
-    xml = SimpleHttp.get sprintf(ERROR_URL, e.id)
-    detail = Nokogiri::XML.parse xml
+    detail =  if options.has_key? :file
+                group
+              else
+                xml = SimpleHttp.get sprintf(ERROR_URL, e.id)
+                Nokogiri::XML.parse xml
+              end
     
     e.process_backtrace!(detail)
 
@@ -94,18 +111,37 @@ end
 
 blames = blames.values
 blames = blames.sort {|a, b| b.score <=> a.score}
-i = 1
+
+blames = blames[0..(options[:number] - 1)] if options.has_key? :number
+blames = blames.reverse if options[:reverse]
+
+i = options[:reverse] ? blames.length : 1 
 
 puts "\nLEADERBOARD OF OPPORTUNITY\n".magenta.underline.bold
 
+if options[:presenter]
+  puts "Press any key to continue..."
+  gets
+end
+
 blames.each do |blame|
+  # clear the screen
+  print "\e[2J\e[f" if options[:presenter]
+
   blame.sort_exceptions!
 
-  puts "\n#{i}.".rjust(3) + " #{blame.author.bold.red.underline} " + "(#{"%.2f" % blame.score})".magenta
+  puts "#{i}.".rjust(3) + " #{blame.author.bold.red.underline} " + "(#{"%.2f" % blame.score})".magenta
   
   blame.exceptions.each do |e|
     puts "\t#{("%.2f" % e.score).rjust(5)}".green +  "\t#{e.notices_count}".yellow + "\t#{e.time_since_last_error / 60} mins ago".blue + "\t#{e.error_message[0..80]} in #{e.file}:#{e.line} (#{e.id})"
   end
 
-  i += 1
+  puts "\n"
+
+  i += options[:reverse] ? -1 : 1
+
+  if options[:presenter]
+    puts "Press any key to continue..."
+    gets
+  end
 end
